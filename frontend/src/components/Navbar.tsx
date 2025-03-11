c92ccf0e8d0a4a554936dd6f6e54b64a44640779
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2025-03-11 19:27:40 +0700

    update: navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import { BsChatRightText } from "react-icons/bs";
+import { IoMdContact } from "react-icons/io";
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -7,28 +9,151 @@ import {
     NavigationMenuList,
     NavigationMenuTrigger,
   } from "@/components/ui/navigation-menu"
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
   
 
-function Navbar() {
+function Navbar({childern}) {
   return (
-    <div className="w-full flex justify-between items-center">
-        <h2>React Chatt App</h2>
-        <ul className="hidden sm:flex">
-          <li><Link to={"/#"}>Profile</Link></li>
-        </ul>
+    <div className="h-dvh flex flex-col justify-between max-h-dvh">
+        <div className="w-full flex justify-between items-center h-[10%]">
+            <h2 className="font-semibold text-xl">React Chatt App</h2>
+            <ul className="hidden sm:flex">
+            <li><Link to={"/#"}>Profile</Link></li>
+            </ul>
 
-        <div className="block sm:hidden">
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                    <NavigationMenuTrigger className="text-base">Menu</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink>Profile</NavigationMenuLink>
-                        <NavigationMenuLink>Setting</NavigationMenuLink>
-                    </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+            <div className="block sm:hidden">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                        <NavigationMenuTrigger className="text-base">Menu</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <NavigationMenuLink href="/#">Profile</NavigationMenuLink>
+                            <NavigationMenuLink>Setting</NavigationMenuLink>
+                        </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+        </div>
+        <div className="flex flex-col w-full max-h-[80%] overflow-auto">
+            {/* {childern} */}
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+            <div>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </div>
+        </div>
+        <div className="flex w-full justify-center items-end space-x-4 h-[10%]">
+            <Link to={"/#"} className="flex flex-col items-center"><BsChatRightText className="text-xl" />Chats</Link>
+            <Link to={"/#"} className="flex flex-col items-center"><IoMdContact  className="text-2xl" />Contacts</Link>
         </div>
     </div>
   )
